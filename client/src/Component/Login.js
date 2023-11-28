6562de9bf6a76b76c29c8935cf7e801ead9f91a8
Atish Chandole <auto91akc@gmail.com>
2023-11-28 18:00:59 +0530

    Update api

--- client/src/Component/Login.js
+++ client/src/Component/Login.js
@@ -51,7 +51,7 @@ const Login = ()=>{
         try
         {          
 
-                const response = await fetch(`http://localhost/Projects/techSpawnMachineTest/server/controller/authController`,{
+                const response = await fetch(`http://atish-backend.byethost7.com/server/controller/authController`,{
                     method:'POST',                             
                     headers:{
                         'Content-Type': 'application/json',
