f43766a130b6e620a45294249d2e4d63a21ac107
Atish Chandole <auto91akc@gmail.com>
2023-11-29 00:13:49 +0530

    SSL fixed8

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/api',
+    '/server/controller/authController',
     createProxyMiddleware({
       target: 'https://atish-backend.byethost7.com',
       secure:false,
